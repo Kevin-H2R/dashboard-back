a80bce2c0f73d9af892795e47751fe451c85e02a
Kevin <herrbach.kevin@gmail.com>
2022-03-17 17:50:19 +0900

    Updates data in crossfit_session table when requested

--- routes/crossfit.js
+++ routes/crossfit.js
@@ -1,37 +1,71 @@
 import axios from 'axios'
 import { parse } from 'node-html-parser';
+import pool from '../services/mariadb.js'
 
-export default function(app){
-    app.get('/crossfit', (req, res) => {
+export default function (app) {
+    app.get('/crossfit', async (req, res) => {
+        const existingSession = await pool.queryResult('SELECT * FROM crossfit_session WHERE DATE(date) = CURDATE()')
+        if (existingSession.length > 0) {
+            res.json({ title: existingSession[0].title, description: existingSession[0].description })
+            return
+        }
         axios.get("http://crossfitzone.cafe24.com/wod.php")
-          .then(response => {
-            const root = parse(response.data)
-            const wod = root.querySelectorAll('.today_wod')
-            const node = wod[wod.length - 1]
-            const titleTag = node.getElementsByTagName('dt')[0]
-            const title = titleTag.text
-            const descriptionTag = node.getElementsByTagName('dd')[1]
-            const description = descriptionTag.innerHTML.trim()
-            res.json({title: title, description: description})
-          })
-      })
-      
-      app.get('/crossfit/login', (req, res) => {
-        axios.post('http://crossfitzone.cafe24.com/ajax/login.php', new URLSearchParams({lg: 'kebinou', pwd: '2Q34,7*|'}), {withCredentials: true})
-          .then(response => {
-            res.json(response.headers['set-cookie'][0])
-          })
-      })
-      
-      app.post('/crossfit/register', (req, res) => {
+            .then(async response => {
+                const root = parse(response.data)
+                const wod = root.querySelectorAll('.today_wod')
+                const node = wod[wod.length - 1]
+                const titleTag = node.getElementsByTagName('dt')[0]
+                const title = titleTag.text
+                const descriptionTag = node.getElementsByTagName('dd')[1]
+                const description = descriptionTag.innerHTML.trim()
+                await pool.query(`INSERT INTO crossfit_session (date, title, description) VALUES (NOW(), "${title}", "${description}")`)
+                res.json({ title: title, description: description })
+            })
+    })
+
+    app.get('/crossfit/login', async (req, res) => {
+        axios.post('http://crossfitzone.cafe24.com/ajax/login.php', new URLSearchParams({ lg: 'kebinou', pwd: '2Q34,7*|' }), { withCredentials: true })
+            .then(async response => {
+                let session = await pool.queryResult("SELECT * FROM crossfit_session WHERE DATE(date) = CURDATE()")
+                session = session.shift()
+                let registered = false
+                let time = null;
+                if (session['attended_time'] !== null) {
+                    let date = new Date(session['attended_time'])
+                    time = date.getHours() - 12
+                    registered = true
+                }
+                res.json({ cookie: response.headers['set-cookie'][0], registered: registered, time: time })
+            })
+    })
+
+    app.post('/crossfit/register', (req, res) => {
         const cookie = req.body.cookie
         const time = req.body.time
         axios.post('http://crossfitzone.cafe24.com/class_requestAct2_new.php',
-                    new URLSearchParams({time: time, type: 1, weekType: 1}),
-                    {withCredentials: true, headers: {common: {'Cookie': cookie}}})
-        .then(response => {
-          console.log(response)
-          res.json(true)
-        })
-      })
+            new URLSearchParams({ time: time, type: 1, weekType: 1 }),
+            { withCredentials: true, headers: { common: { 'Cookie': cookie } } })
+            .then(async () => {
+                const date = new Date()
+                date.setHours(time + 12)
+                const month = (date.getMonth() + 1).toString().padStart(2, '0')
+                const formatedDate = `${date.getFullYear()}-${month}-${date.getDate()} ${date.getHours()}:00:00`
+                await pool.query(`UPDATE crossfit_session SET attended_time = "${formatedDate}" WHERE DATE(date) = CURDATE()`)
+                res.json(true)
+            })
+    })
+
+    app.post('/crossfit/cancel', (req, res) => {
+        const cookie = req.body.cookie
+        const time = req.body.time
+        axios.post('http://crossfitzone.cafe24.com/ajax/cancelreq2.php',
+        new URLSearchParams({ time: time}),
+            // {time: time},
+            { withCredentials: true, headers: { common: { 'Cookie': cookie } } })
+            .then(async (result) => {
+                console.log(result)
+                await pool.query('UPDATE crossfit_session SET attended_time = NULL WHERE DATE(date) = CURDATE()')
+                res.json(true)
+            })
+    })
 }
--- routes/todo.js
+++ routes/todo.js
@@ -4,39 +4,39 @@ export default function (app) {
     app.get('/todo', async (req, res) => {
         let rows = await pool.queryResult('SELECT * FROM todo ORDER BY creation_date DESC')
         rows.map(r => {
-          r.done = r.done === 1
-          return r
+            r.done = r.done === 1
+            return r
         })
         res.json({ tasks: rows })
-      })
-      
-      app.post('/todo', async (req, res) => {
+    })
+
+    app.post('/todo', async (req, res) => {
         const name = req.body.name
         let result = await pool.queryResult(`INSERT INTO todo (title, description, done, creation_date, update_date)
         VALUES ("${name}", "", FALSE, NOW(), NOW())`)
         let rows = await pool.queryResult(`SELECT * FROM todo WHERE id = ${result.insertId}`)
         rows[0].done = rows[0].done === 1
         res.json({ task: rows[0] })
-      })
-      
-      app.post('/todo/toggle', async (req, res) => {
+    })
+
+    app.post('/todo/toggle', async (req, res) => {
         const done = req.body.done
         const id = req.body.id
         let conn;
         try {
-          conn = await pool.getConnection();
-          await conn.query(`UPDATE todo SET done = ${done} WHERE id = ${id}`)
-          res.json(true)
+            conn = await pool.getConnection();
+            await conn.query(`UPDATE todo SET done = ${done} WHERE id = ${id}`)
+            res.json(true)
         } catch (err) {
-          console.log(err)
+            console.log(err)
         } finally {
-          if (conn) conn.release()
+            if (conn) conn.release()
         }
-      })
-      
-      app.post('/todo/delete', async (req, res) => {
+    })
+
+    app.post('/todo/delete', async (req, res) => {
         const id = req.body.id
         await pool.query(`DELETE FROM todo WHERE id = ${id}`)
         res.json(true)
-      })
+    })
 }
